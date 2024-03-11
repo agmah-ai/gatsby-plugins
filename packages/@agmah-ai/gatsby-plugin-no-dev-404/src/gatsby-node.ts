a5251e6c8ea12fe8088e7a2b4ef89f73dcde4e58
Arpit Aggarwal <a.aggarwal@agmah.ai>
2024-03-11 14:36:17 +0100

    feat(gatsby-plugin-no-dev-404): Add package

---
+++ packages/@agmah-ai/gatsby-plugin-no-dev-404/src/gatsby-node.ts
@@ -0,0 +1,12 @@
+import type { CreatePageArgs } from "gatsby";
+
+// @see https://github.com/gatsbyjs/gatsby/issues/16112
+export const onCreatePage = ({ page, actions }: CreatePageArgs) => {
+  if (process.env.NODE_ENV !== `production` && page.path === `/404/`) {
+    const { createPage } = actions;
+    // Make the 404 page match everything client side.
+    // This will be used as fallback if more specific pages are not found
+    page.matchPath = `/*`;
+    createPage(page);
+  }
+};
