76f1355350d570ba159e0beeba2312eb9dfd1c41
Arpit Aggarwal <a.aggarwal@agmah.ai>
2024-03-12 15:12:06 +0100

    feat(gatsby-plugin-remove-fingerprints): Add package

---
+++ packages/@agmah-ai/gatsby-plugin-remove-fingerprints/src/gatsby-node.ts
@@ -0,0 +1,21 @@
+import type { CreateWebpackConfigArgs } from "gatsby";
+
+export const onCreateWebpackConfig = ({
+  stage,
+  getConfig,
+  actions,
+}: CreateWebpackConfigArgs) => {
+  if (stage === "build-javascript") {
+    const newWebpackConfig = {
+      ...getConfig(),
+      output: {
+        filename: `[name].js`,
+        chunkFilename: `[name].js`,
+        path: getConfig().output.path,
+        publicPath: getConfig().output.publicPath,
+      },
+    };
+
+    actions.replaceWebpackConfig(newWebpackConfig);
+  }
+};
