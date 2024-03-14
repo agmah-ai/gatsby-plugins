e7a756f7e1128b77c4843c1d1e53c30f44adf43d
Arpit Aggarwal <a.aggarwal@agmah.ai>
2024-03-14 10:58:40 +0100

    feat(gatsby-plugin-no-comments-license): Add package

---
+++ packages/@agmah-ai/gatsby-plugin-no-comments-license/src/gatsby-node.ts
@@ -0,0 +1,28 @@
+import type { CreateWebpackConfigArgs } from "gatsby";
+
+export const onCreateWebpackConfig = ({ actions, stage, getConfig }: CreateWebpackConfigArgs) => {
+  if (stage === "build-javascript") {
+    const config = getConfig();
+
+    // Find the minimizer in the webpack configuration
+    const minifyJSIndex = config.optimization.minimizer.findIndex(
+      (minimizer) => minimizer.constructor.name === "TerserPlugin",
+    );
+
+    // Check if the TerserPlugin is found
+    if (minifyJSIndex !== -1) {
+      const terserPluginOptions = config.optimization.minimizer[minifyJSIndex].options;
+
+      terserPluginOptions.terserOptions = {
+        ...terserPluginOptions.terserOptions,
+        format: {
+          comments: false,
+        },
+      };
+
+      terserPluginOptions.extractComments = false;
+
+      actions.replaceWebpackConfig(config);
+    }
+  }
+};
